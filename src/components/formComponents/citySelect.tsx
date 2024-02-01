e196fbc5854cb5beaecb740fc3dc94299bb340f1
Lucas-Benfica <lucassoaresbenfica@gmail.com>
2024-02-01 16:04:08 -0300

    feat: pokeApi cities

--- src/components/formComponents/citySelect.tsx
+++ src/components/formComponents/citySelect.tsx
@@ -1,5 +1,6 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { FormSelect, OptionList, Option } from "./styles";
+const URL = process.env.NEXT_PUBLIC_API_LINK_2;
 
 interface CitySelectProps {
     setValue: any;
@@ -8,32 +9,44 @@ interface CitySelectProps {
 
 export default function CitySelect(props: CitySelectProps) {
 
-    const { setValue } = props;
+    const { setValue, region } = props;
 
     const [isOpen, setIsOpen] = useState(false);
     const [selectedOption, setSelectedOption] = useState('');
+    const [cityList, setCityList] = useState<{ name: string, url: string }[]>()
 
-    function toggleOptions(){
+    useEffect(() => {
+        if (!region) return;
+        fetch(`${URL}/region/${region}`)
+            .then(response => response.json())
+            .then(regionData => {
+                const locations: { name: string, url: string }[] = regionData.locations;
+                const cities: { name: string, url: string }[] = locations.filter(city => city.name.includes('city'));
+                setCityList(cities);
+            })
+            .catch(error => console.error('Erro:', error));
+    }, [region]);
+
+    function toggleOptions() {
         setIsOpen(!isOpen);
     };
 
-    function handleOptionClick(value: string){
+    function handleOptionClick(value: string) {
         setSelectedOption(value);
         setValue("city", value);
         setIsOpen(false);
     };
 
     return (
-        <FormSelect onClick={toggleOptions} open = {isOpen}>
+        <FormSelect onClick={toggleOptions} open={isOpen}>
             {selectedOption ? selectedOption : 'Cidade'}
             <span>{'>'}</span>
             {isOpen && (
                 <OptionList>
                     <Option onClick={() => handleOptionClick('')}>-</Option>
-                    <Option onClick={() => handleOptionClick('Kanto')}>Kanto</Option>
-                    <Option onClick={() => handleOptionClick('Johto')}>Johto</Option>
-                    <Option onClick={() => handleOptionClick('Hoenn')}>Hoenn</Option>
-                    <Option onClick={() => handleOptionClick('Sinnoh')}>Sinnoh</Option>
+                    {cityList && cityList.map((city, index) => (
+                        <Option key={index} onClick={() => handleOptionClick(city.name)}>{city.name}</Option>
+                    ))}
                 </OptionList>
             )}
         </FormSelect>
