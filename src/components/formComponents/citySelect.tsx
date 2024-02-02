ea41037b9a53b8e90ac8bb423e975978769bb546
Lucas-Benfica <lucassoaresbenfica@gmail.com>
2024-02-02 10:39:48 -0300

    feat: cities notification

--- src/components/formComponents/citySelect.tsx
+++ src/components/formComponents/citySelect.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import { FormSelect, OptionList, Option } from "./styles";
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 const URL = process.env.NEXT_PUBLIC_API_LINK_2;
 
 interface CitySelectProps {
@@ -17,14 +19,29 @@ export default function CitySelect(props: CitySelectProps) {
 
     useEffect(() => {
         if (!region) return;
-        fetch(`${URL}/region/${region}`)
-            .then(response => response.json())
-            .then(regionData => {
-                const locations: { name: string, url: string }[] = regionData.locations;
-                const cities: { name: string, url: string }[] = locations.filter(city => city.name.includes('city'));
-                setCityList(cities);
-            })
-            .catch(error => console.error('Erro:', error));
+        const fetchData = async () => {
+            try {
+                await toast.promise(
+                    fetch(`${URL}/region/${region}`),
+                    {
+                        pending: `Searching for ${region} cities...`,
+                        success: `${region} Cities fetched successfully!`,
+                        error: `Failed to fetch ${region} cities!`
+                    }
+                )
+                .then(response => response.json())
+                .then(regionData => {
+                    const locations: { name: string, url: string }[] = regionData.locations;
+                    const cities: { name: string, url: string }[] = locations.filter(city => city.name.includes('city'));
+                    setCityList(cities);
+                })
+
+            } catch (error) {
+                console.error('Erro:', error);
+            }
+        };
+
+        fetchData();
     }, [region]);
 
     function toggleOptions() {
